87f07db14f837af716a69aabb0779b5ce6019467
AnneBirdie <149617951+AnneBirdie@users.noreply.github.com>
2024-04-14 16:01:11 +0500

    Update feedback_wish.js

--- newProjectWedding/scripts/feedback_wish.js
+++ newProjectWedding/scripts/feedback_wish.js
@@ -10,11 +10,13 @@
   form.addEventListener('submit', e => {
     e.preventDefault()
 
-      alert('Ваше поздравление принято и будет опубликовано после проверки');
-      nameWish.value = '';
-      commentWish.value = '';
       fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
+
+    
+      alert('Ваше поздравление принято и будет опубликовано после проверки');
+      nameWish.value = '';
+      commentWish.value = '';
 
   })
